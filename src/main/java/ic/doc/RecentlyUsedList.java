2a9a4f3a11821bd826b5cdd0dd7979c1a5f865e0
Tristan Clark <tc4518@point41.doc.ic.ac.uk>
2019-01-31 12:58:36 +0000

    OS TC code reformatted to google style sheet(downloaded style sheet)

--- src/main/java/ic/doc/RecentlyUsedList.java
+++ src/main/java/ic/doc/RecentlyUsedList.java
@@ -6,30 +6,30 @@ import java.util.List;
 
 public class RecentlyUsedList<E> extends ArrayList<E> {
 
-    @Override
-    public boolean add(E element) {
-        try {
-            if (contains(element)) {
-                remove(element);
-                add(0, element);
-                return true;
-            } else {
-                add(0, element);
-                return true;
-            }
-        } catch (IndexOutOfBoundsException e) {
-            return false;
-        }
+  @Override
+  public boolean add(E element) {
+    try {
+      if (contains(element)) {
+        remove(element);
+        add(0, element);
+        return true;
+      } else {
+        add(0, element);
+        return true;
+      }
+    } catch (IndexOutOfBoundsException e) {
+      return false;
     }
+  }
 
-    @Override
-    public void add(int index, E element) {
-        if (index != 0) {
-            throw new IndexOutOfBoundsException();
-        } else {
-            super.add(0, element);
-        }
+  @Override
+  public void add(int index, E element) {
+    if (index != 0) {
+      throw new IndexOutOfBoundsException();
+    } else {
+      super.add(0, element);
     }
+  }
 
 
 }
--- src/test/java/ic/doc/RecentlyUsedListTest.java
+++ src/test/java/ic/doc/RecentlyUsedListTest.java
@@ -10,64 +10,64 @@ import static org.hamcrest.core.Is.is;
 
 public class RecentlyUsedListTest {
 
-    @Test
-    public void listInitialisedAsEmpty() {
-        assertTrue(new RecentlyUsedList<Integer>().size() == 0);
+  @Test
+  public void listInitialisedAsEmpty() {
+    assertTrue(new RecentlyUsedList<Integer>().size() == 0);
+  }
+
+  @Test
+  public void canAddItems() {
+    List<Integer> list = new RecentlyUsedList<>();
+    assertTrue(list.add(new Integer(5)));
+  }
+
+  @Test
+  public void doesNotAllowAddingAtIndexOtherThanZero() {
+    List<Integer> list = new RecentlyUsedList<>();
+    list.add(new Integer(5));
+    list.add(new Integer(2));
+    list.add(new Integer(1));
+    try {
+      list.add(2, new Integer(3));
+    } catch (IndexOutOfBoundsException e) {
     }
+  }
 
-    @Test
-    public void canAddItems() {
-        List<Integer> list = new RecentlyUsedList<>();
-        assertTrue(list.add(new Integer(5)));
-    }
+  @Test
+  public void canRetrieveFromList() {
+    List<Integer> list = new RecentlyUsedList<>();
+    list.add(new Integer(5));
+    list.add(new Integer(2));
+    list.add(new Integer(1));
+    list.add(new Integer(4));
+    assertThat(list.get(0), is(4));
+    assertThat(list.get(1), is(1));
+    assertThat(list.get(2), is(2));
+    assertThat(list.get(3), is(5));
+  }
 
-    @Test
-    public void doesNotAllowAddingAtIndexOtherThanZero() {
-        List<Integer> list = new RecentlyUsedList<>();
-        list.add(new Integer(5));
-        list.add(new Integer(2));
-        list.add(new Integer(1));
-        try {
-            list.add(2, new Integer(3));
-        } catch (IndexOutOfBoundsException e) {
-        }
-    }
-
-    @Test
-    public void canRetrieveFromList() {
-        List<Integer> list = new RecentlyUsedList<>();
-        list.add(new Integer(5));
-        list.add(new Integer(2));
-        list.add(new Integer(1));
-        list.add(new Integer(4));
-        assertThat(list.get(0), is(4));
-        assertThat(list.get(1), is(1));
-        assertThat(list.get(2), is(2));
-        assertThat(list.get(3), is(5));
-    }
-
-    @Test
-    public void cannotAddDuplicates() {
-        List<Integer> list = new RecentlyUsedList<>();
-        list.add(new Integer(5));
-        list.add(new Integer(5));
-        list.add(new Integer(5));
-        list.add(new Integer(6));
-        list.add(new Integer(6));
-        list.add(new Integer(7));
-        assertThat(list.size(), is(3));
-    }
+  @Test
+  public void cannotAddDuplicates() {
+    List<Integer> list = new RecentlyUsedList<>();
+    list.add(new Integer(5));
+    list.add(new Integer(5));
+    list.add(new Integer(5));
+    list.add(new Integer(6));
+    list.add(new Integer(6));
+    list.add(new Integer(7));
+    assertThat(list.size(), is(3));
+  }
 
 
-    @Test
-    public void worksForAnyObject() {
-        List<String> list = new RecentlyUsedList<>();
-        list.add("Hello");
+  @Test
+  public void worksForAnyObject() {
+    List<String> list = new RecentlyUsedList<>();
+    list.add("Hello");
 
-        List<List<Integer>> listOuter = new RecentlyUsedList<>();
-        List<Integer> listInner = new RecentlyUsedList<>();
-        listOuter.add(listInner);
-    }
+    List<List<Integer>> listOuter = new RecentlyUsedList<>();
+    List<Integer> listInner = new RecentlyUsedList<>();
+    listOuter.add(listInner);
+  }
 
 
 }
