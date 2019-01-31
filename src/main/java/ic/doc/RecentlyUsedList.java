6378881adfb5084816bdeebc1a04741704d2ed02
Tristan Clark <tc4518@point41.doc.ic.ac.uk>
2019-01-31 12:27:09 +0000

    OS TC add method created and tested

--- src/main/java/ic/doc/RecentlyUsedList.java
+++ src/main/java/ic/doc/RecentlyUsedList.java
@@ -1,4 +1,26 @@
 package ic.doc;
 
-public class RecentlyUsedList {
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecentlyUsedList<E> extends ArrayList<E> {
+
+    @Override
+    public boolean add(E element) {
+        try {
+            if(contains(element)) {
+                remove(element);
+                add(0, element);
+                return true;
+            } else {
+                 add(0, element);
+                return true;
+            }
+        } catch(IndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+
+
 }
